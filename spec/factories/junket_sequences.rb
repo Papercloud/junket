dfababaa5795e3efdf0e6358c15f3d7eb0390e78
Karl Glaser <kg@papercloud.com.au>
2015-02-26 14:53:58 +1100

    factories

--- spec/factories/junket_sequences.rb
+++ spec/factories/junket_sequences.rb
@@ -22,21 +22,23 @@ FactoryGirl.define do
     after(:create) do |t, _evaluator|
       # create two action templates.
       2.times do |n|
-        FactoryGirl.create(:junket_sequence_action_time, action_template: FactoryGirl.create(:junket_action_template), sequence_template: t, position: n)
+        FactoryGirl.create(:junket_action_template, sequence_template: t, position: n)
       end
     end
   end
 
   factory :junket_action_template, class: 'Junket::ActionTemplate' do
     sequence(:name) { |n| "A Template #{n}" }
-
+    duration_since_previous 10.minutes
+    position 0
     send_email true
     send_sms true
     email_subject 'Exciting New Features!'
     email_body 'Hi, check out our new features!'
     sms_body 'We have new features!'
+    association :sequence_template, factory: :junket_sequence_template
 
-    # type 'RecallSmsActionTemplate'
+    type 'TestActionTemplateSubclass'
   end
 
   factory :junket_action, class: 'Junket::Action' do
@@ -46,16 +48,9 @@ FactoryGirl.define do
     state 'scheduled'
   end
 
-  factory :junket_sequence_action_time, class: 'Junket::SequenceActionTime' do
-    duration_since_previous 10.minutes
-    association :sequence_template, factory: :junket_sequence_template
-    association :action_template, factory: :junket_action_template
-    action_template_type 'RecallSmsActionTemplate'
-  end
-
-  factory :junket_sequence, class: 'Junket::Sequence' do
-    object { User.create(email: 'a@a.com') }
-    owner { User.create(email: 'a@a.com') }
-    association :sequence_template, factory: :junket_sequence_template
-  end
+  # factory :junket_sequence, class: 'Junket::Sequence' do
+  #   object { User.create(email: 'a@a.com') }
+  #   owner { User.create(email: 'a@a.com') }
+  #   association :sequence_template, factory: :junket_sequence_template
+  # end
 end

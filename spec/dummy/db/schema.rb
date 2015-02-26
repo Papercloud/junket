bcb0d93cfbd5ae6c9539fcce4060365dbb856bc8
John Servinis <john.a.servinis@gmail.com>
2015-02-26 16:42:30 +1100

    schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,87 +11,84 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_150_224_032_103) do
+ActiveRecord::Schema.define(version: 20150224032103) do
 
-  create_table 'junket_action_templates', force: true do |t|
-    t.string 'name'
-    t.string 'campaign_name'
-    t.string 'email_subject'
-    t.text 'email_body'
-    t.text 'sms_body'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string 'type'
-    t.integer 'sequence_template_id'
-    t.integer 'run_after_duration',   default: 0, null: false
-    t.integer 'position',             default: 0, null: false
+  create_table "junket_action_templates", force: true do |t|
+    t.string   "name"
+    t.string   "campaign_name"
+    t.string   "email_subject"
+    t.text     "email_body"
+    t.text     "sms_body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "type"
+    t.integer  "run_after_duration", default: 0, null: false
+    t.integer  "position",           default: 0, null: false
   end
 
-  add_index 'junket_action_templates', ['sequence_template_id'], name: 'index_junket_action_templates_on_sequence_template_id'
-
-  create_table 'junket_actions', force: true do |t|
-    t.integer 'action_template_id', null: false
-    t.integer 'object_id',          null: false
-    t.string 'object_type',        null: false
-    t.string 'state',              null: false
-    t.datetime 'run_datetime',       null: false
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "junket_actions", force: true do |t|
+    t.integer  "action_template_id", null: false
+    t.integer  "object_id",          null: false
+    t.string   "object_type",        null: false
+    t.string   "state",              null: false
+    t.datetime "run_datetime",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index 'junket_actions', ['action_template_id'], name: 'index_junket_actions_on_action_template_id'
-  add_index 'junket_actions', %w(object_id object_type), name: 'index_junket_actions_on_object_id_and_object_type'
+  add_index "junket_actions", ["action_template_id"], name: "index_junket_actions_on_action_template_id"
+  add_index "junket_actions", ["object_id", "object_type"], name: "index_junket_actions_on_object_id_and_object_type"
 
-  create_table 'junket_filter_conditions', force: true do |t|
-    t.integer 'filter_id'
-    t.integer 'campaign_id'
-    t.string 'value'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "junket_filter_conditions", force: true do |t|
+    t.integer  "filter_id"
+    t.integer  "campaign_id"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index 'junket_filter_conditions', ['campaign_id'], name: 'index_junket_filter_conditions_on_campaign_id'
-  add_index 'junket_filter_conditions', ['filter_id'], name: 'index_junket_filter_conditions_on_filter_id'
+  add_index "junket_filter_conditions", ["campaign_id"], name: "index_junket_filter_conditions_on_campaign_id"
+  add_index "junket_filter_conditions", ["filter_id"], name: "index_junket_filter_conditions_on_filter_id"
 
-  create_table 'junket_filters', force: true do |t|
-    t.string 'name'
-    t.string 'term'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string 'value_type'
+  create_table "junket_filters", force: true do |t|
+    t.string   "name"
+    t.string   "term"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "value_type"
   end
 
-  create_table 'junket_recipients', force: true do |t|
-    t.integer 'target_id'
-    t.string 'target_type'
-    t.integer 'campaign_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string 'email_third_party_id'
+  create_table "junket_recipients", force: true do |t|
+    t.integer  "target_id"
+    t.string   "target_type"
+    t.integer  "campaign_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email_third_party_id"
   end
 
-  add_index 'junket_recipients', ['campaign_id'], name: 'index_junket_recipients_on_campaign_id'
-  add_index 'junket_recipients', ['email_third_party_id'], name: 'index_junket_recipients_on_email_third_party_id'
-  add_index 'junket_recipients', %w(target_id target_type), name: 'index_junket_recipients_on_target_id_and_target_type'
+  add_index "junket_recipients", ["campaign_id"], name: "index_junket_recipients_on_campaign_id"
+  add_index "junket_recipients", ["email_third_party_id"], name: "index_junket_recipients_on_email_third_party_id"
+  add_index "junket_recipients", ["target_id", "target_type"], name: "index_junket_recipients_on_target_id_and_target_type"
 
-  create_table 'junket_sequence_templates', force: true do |t|
-    t.string 'name',                             null: false
-    t.integer 'owner_id'
-    t.string 'owner_type'
-    t.string 'access_level', default: 'private', null: false
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "junket_sequence_templates", force: true do |t|
+    t.string   "name",                             null: false
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "access_level", default: "private", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index 'junket_sequence_templates', %w(owner_id owner_type), name: 'index_junket_sequence_templates_on_owner_id_and_owner_type'
+  add_index "junket_sequence_templates", ["owner_id", "owner_type"], name: "index_junket_sequence_templates_on_owner_id_and_owner_type"
 
-  create_table 'users', force: true do |t|
-    t.string 'email'
-    t.string 'mobile'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "mobile"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

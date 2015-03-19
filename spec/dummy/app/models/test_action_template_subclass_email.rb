b40f6dbbdb493883a6b13ce60336fd764d0f4048
Karl Glaser <kg@papercloud.com.au>
2015-03-20 10:50:16 +1100

    specs passing again, by putting validation on subclass which may not be the best way.

--- spec/dummy/app/models/test_action_template_subclass_email.rb
+++ spec/dummy/app/models/test_action_template_subclass_email.rb
@@ -1,4 +1,10 @@
 class TestActionTemplateSubclassEmail < Junket::ActionTemplate
+  validates :email_subject, presence: true
+  validates :email_subject, length: { maximum: 78 }
+  validates :email_body, presence: true
+  validates :sms_body, presence: true
+  validates :sms_body, length: { maximum: 160 }
+
   def create_action_for(struct)
     super(struct)
   end
--- spec/dummy/app/models/test_action_template_subclass_sms.rb
+++ spec/dummy/app/models/test_action_template_subclass_sms.rb
@@ -1,4 +1,7 @@
 class TestActionTemplateSubclassSms < Junket::ActionTemplate
+  validates :sms_body, presence: true
+  validates :sms_body, length: { maximum: 160 }
+
   def create_action_for(struct)
     super(struct)
   end
